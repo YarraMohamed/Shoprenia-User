9a2516136f385b26feffdc8d2683bf617c98b241
YarraMohamed <yarramohammed02@gmail.com>
2025-05-27 21:18:25 +0300

    fitst commit

---
+++ Shoprenia/ShopreniaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopreniaApp.swift
+//  Shoprenia
+//
+//  Created by Yara Mohamed on 27/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct ShopreniaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
